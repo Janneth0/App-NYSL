23f978c4a61ef6fcf543f8bba2ee56fb9b106023
Janneth0 <churajanneth@gmail.com>
2020-11-25 01:18:09 -0300

    continue

--- src/main.js
+++ src/main.js
@@ -12,6 +12,19 @@ Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: "AIzaSyAGTLksFu2sx384_OqjmWocS2L5yYOts1M",
+  authDomain: "nysl-app-2020.firebaseapp.com",
+  databaseURL: "https://nysl-app-2020.firebaseio.com",
+  projectId: "nysl-app-2020",
+  storageBucket: "nysl-app-2020.appspot.com",
+  messagingSenderId: "947170369073",
+  appId: "1:947170369073:web:9047bf21c2bf248008bf10",
+  measurementId: "G-4SCN8VMXSR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
   router,
   store,
